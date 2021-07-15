17457f575c554fd7bd07ffac7a8ad31b77588372
Walter Corrales <corraleswalter@live.com>
2021-07-15 13:04:55 -0600

    [✨] creating http client with axios and creating task service

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,9 +1,25 @@
-import { TaskType } from "../types/task.type";
+import { TaskType } from '../types/task.type';
+import http from './http';
 
 export const TaskService = {
-    getAll() { },
-    save() { },
-    get(taskId: number) { },
-    delete(taskId: number) { },
-    update(taskId: number, data: TaskType) { },
+    async getAll() {
+        const { data } = await http.get<TaskType[]>('/api/task');
+        return data;
+    },
+    async save(body: TaskType) {
+        const response = await http.post<TaskType>('/api/task', body);
+        return response;
+    },
+    async get(taskId: number) {
+        const { data } = await http.get<TaskType>(`/api/task/${taskId}`);
+        return data;
+    },
+    async delete(taskId: number) {
+        const response = await http.delete<{ message: string }>(`/api/task/${taskId}`);
+        return response;
+    },
+    async update(taskId: number, data: TaskType) {
+        const response = await http.put<TaskType>(`/api/task/${taskId}`, data);
+        return response;
+    },
 }
