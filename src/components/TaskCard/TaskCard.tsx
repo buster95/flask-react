4ecded89836837d7d0681fd69106d0f06a95d961
Walter Corrales <corraleswalter@live.com>
2021-07-15 23:30:14 -0600

    [✨] task card adding functionality

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -10,37 +10,38 @@ import CardContent from '@material-ui/core/CardContent';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
+import { TaskType } from '../../types/task.type';
 
 interface TaskCardProps {
-  task: string;
-  description: string;
+  task: TaskType;
+  action: (action: 'update' | 'finish', task: TaskType) => void;
 }
 
 const TaskCard: React.FC<TaskCardProps> = (props) => {
-  const { task, description } = props;
+  const { task, action } = props;
 
   return (
     <Card className="TaskCard" elevation={2}>
-      <CardContent>
+      <CardContent style={{ borderBottom: `8px solid ${task.iscompleted ? 'rgb(96,206,118	)' : 'red'}` }}>
         <div className="TaskCard-content">
           <div>
             <Checkbox></Checkbox>
           </div>
 
           <div style={{ flexGrow: 1, marginLeft: 20 }}>
-            <Typography align="left" variant="body1">{task}</Typography>
-            <Typography align="left" variant="body2" color="textSecondary">{description}</Typography>
+            <Typography align="left" variant="body1">{task.task}</Typography>
+            <Typography align="left" variant="body2" color="textSecondary">{task.description}</Typography>
           </div>
 
           <div className="TaskCard-icons">
             <Tooltip title="Update task" placement="bottom">
-              <IconButton>
+              <IconButton onClick={() => action('update', task)}>
                 <UpdateIcon />
               </IconButton>
             </Tooltip>
 
             <Tooltip title="Finish task" placement="bottom">
-              <IconButton>
+              <IconButton onClick={() => action('finish', task)}>
                 <FinishIcon />
               </IconButton>
             </Tooltip>
