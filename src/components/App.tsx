e24d5b7acca40ea36f172345b37e8326cb200479
Walter Corrales <corraleswalter@live.com>
2021-07-15 23:59:02 -0600

    [✨] implementing task card and task dialog

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import Task from './Task/Task';
+import TaskDialog from './TaskDialog/TaskDialog';
+import { TaskType } from '../types/task.type';
+
+import AddIcon from '@material-ui/icons/AddRounded';
+import DeleteIcon from '@material-ui/icons/DeleteRounded';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { TaskService } from '../services/task.service';
 
 const App: React.FC = () => {
+  const [openAdd, setOpenAdd] = useState(false);
+
+  const handleOnAdd = async (task?: TaskType) => {
+    if (task !== undefined) {
+      if (task.id < 0) {
+        const newTask = (await TaskService.save(task)).data;
+        console.log(newTask)
+      }
+    }
+    setOpenAdd(false);
+  };
+
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ textAlign: 'left', flexGrow: 1 }}>
+            My Awesome App
+          </Typography>
+
+          <IconButton edge="end" color="inherit" style={{ marginLeft: 15 }} aria-label="menu">
+            <DeleteIcon />
+          </IconButton>
+
+          <IconButton edge="end" color="inherit" style={{ marginLeft: 15 }} onClick={() => setOpenAdd(true)}>
+            <AddIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <div className="container">
+        <Task />
+
+        <TaskDialog open={openAdd} onClose={handleOnAdd} task={null} />
+      </div>
     </div>
   );
 }
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Task.scss';
 
-const Task = () => (
-  <div className="Task">
-    Task Component
-  </div>
-);
+import Typography from '@material-ui/core/Typography';
+import TaskCard from '../TaskCard/TaskCard';
+import { TaskType } from '../../types/task.type';
+import TaskDialog from '../TaskDialog/TaskDialog';
+import { TaskService } from '../../services/task.service';
+
+const Task: React.FC = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [task, setTask] = useState<TaskType | null>(null);
+
+  useEffect(() => {
+    TaskService.getAll().then(tasks => {
+      setTasks(tasks);
+    }).catch(error => {
+      console.error(error)
+    });
+  }, []);
+
+  const handleOnClose = (task?: TaskType) => {
+    if (task === undefined) {
+      setOpenDialog(false);
+      return;
+    }
+    TaskService.update(task.id, task).then(() => {
+      setTasks(tasks.map(item => {
+        if (item.id === task.id) {
+          return task;
+        }
+        return item;
+      }));
+      setOpenDialog(false);
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
+  const handleCardAction = (action: 'update' | 'finish', returnedTask: TaskType) => {
+    switch (action) {
+      case 'update':
+        setTask(returnedTask);
+        setOpenDialog(true)
+        break;
+
+      case 'finish':
+        setTask(returnedTask);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div className="Task">
+      <Typography variant="h5" style={{ marginBottom: 20 }}>Task List</Typography>
+      <TaskDialog open={openDialog} task={task} onClose={handleOnClose} />
+
+      {tasks.map((task: TaskType, index: number) => (
+        <TaskCard key={index} task={task} action={handleCardAction} />
+      ))}
+    </div>
+  );
+}
 
 export default Task;
